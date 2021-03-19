8c43d9f0faad68d9e237734ebe54aadd0a278588
francescapetra <francescalaurapetra@gmail.com>
2021-03-19 17:52:54 +0100

    snack 1 e 2

--- Snack -1/js/script.js
+++ Snack -1/js/script.js
@@ -1,31 +1,31 @@
-// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+ // nome e peso. Stampare a schermo la bici con peso minore
+const bike = [
+  {
+    nome: "Atala",
+    peso: 5000,
+  },
+  {
+    nome: "Maverick",
+    peso: 8000,
+  },
+  {
+    nome: "Fondriest",
+    peso: 7000,
+  },
+];
 
-// // creo array oggetti
-// var bike = [
-//   {
-//     nome: "Atala",
-//     peso: 4000,
-//   },
-//   {
-//     nome: "Maverick",
-//     peso: 6000,
-//   },
-//   {
-//     nome: "Moser",
-//     peso: 3000,
-//   },
-// ];
-// // creo una variabile fuori dove prendo l'indice dell'objt
-// var lightBike = bike[0];
-// // faccio cilare gli elementi
-// for (var i = 0; i < bike.length; i++) {
-//   console.log(bike[i]);
-// // se la chiave peso dell'oggetto bike[i] è inferiore alla ligthbike peso
-//   if (bike[i].peso < lightBike.peso) {
-//     // la bike ligth diventa questa
-//     lightBike = bike[i];
-//   }
-//
-// }
-//
-// console.log("La bici più leggera è marca " + lightBike.nome + " e pesa " + lightBike.peso);
+// creo una variabile fuori dove prendo l'indice dell'objt
+let lightBike = null;
+// faccio cilare gli elementi
+for (let i = 0; i < bike.length; i++) {
+  console.log(bike[i]);
+// se la chiave peso dell'oggetto bike[i] è inferiore alla ligthbike peso
+  if (lightBike == null ||  lightBike.peso > bike[i].peso ) {
+    // la bike ligth diventa questa
+    lightBike = bike[i];
+  }
+}
+const {nome,peso} = lightBike;
+console.log(
+`La bici più leggera è: ${nome} e pesa ${peso} `);
--- Snack -2/js/script.js
+++ Snack -2/js/script.js
@@ -1,56 +1,48 @@
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e
+//stampiamo tutto in console.
 // creo array oggetti
-// var squadre = [
-//   {
-//     nome: "Atalanta",
-//     punti: 0,
-//     falli : 0,
-//   },
-//   {
-//     nome: "Roma",
-//     punti: 0,
-//     falli : 0,
-//   },
-//   {
-//     nome: "Benevento",
-//     punti: 0,
-//     falli : 0,
-//   },
-// ];
-// // 2 step:
-// // Generare numeri random al posto degli 0 nelle proprietà:
-// // Punti fatti e falli subiti.
-// // creo una variabile fuori dove prendo l'indice dell'objt
-//
-// // var squadre[i];
-// for (var key in squadre) {
-//   console.log([key]);
-//   console.log(squadre[key].punti, squadre[key].falli)
-//
-//   if (squadre[key].punti == 0) {
-//     squadre[key].punti = randomGeneratore(1, 10);
-//   }
-//   if (squadre[key].falli == 0) {
-//     squadre[key].falli = randomGeneratore(1, 10);
-//   }
-// }
-// for (var i = 0; i < squadre.length; i++) {
-//   console.log(squadre);
-// }
-//
-//
-// function randomGeneratore(min, max){
-//   var numeriCpu = Math.floor(Math.random() * max) + 1;
-//   return numeriCpu;
-// }
-// // 3 step:
-// // Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-// //
-// var nuovoArray = [];
-//
-// for (var key in squadre) {
-// nuovoArray.push(squadre[key].nome, squadre[key].falli)
-// }
-// console.log(nuovoArray);
+const squadre = [
+  {
+    nome: "Atalanta",
+    punti: 0,
+    falli : 0,
+  },
+  {
+    nome: "Roma",
+    punti: 0,
+    falli : 0,
+  },
+  {
+    nome: "Benevento",
+    punti: 0,
+    falli : 0,
+  },
+];
+const {nome,punti,falli} = squadre;
+console.log(squadre);
+//function
+function randomGeneratore(min, max){
+  let numeriCpu = Math.floor(Math.random() * max) + 1;
+  return numeriCpu;
+}
+// ciclo
+for (let i = 0; i < squadre.length; i++) {
+  squadre[i].punti = randomGeneratore(1,10);
+  squadre[i].falli = randomGeneratore(1,10);
+}
+// nuovoArray
+const nuovoArray = [];
+
+for (let i = 0; i < squadre.length; i++) {
+
+const {nome,falli} = squadre[i];
+nuovoArray.push({
+  nome: nome,
+  falli :falli,
+});
+}
+console.log(nuovoArray);
