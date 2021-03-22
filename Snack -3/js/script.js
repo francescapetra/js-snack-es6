3f3346e2bdf9996e36a3e550ec33dda3b2dce22e
francescapetra <francescalaurapetra@gmail.com>
2021-03-22 18:38:33 +0100

    riscritte leggibili

--- Snack -3/js/script.js
+++ Snack -3/js/script.js
@@ -1,43 +1,53 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-//
+// // // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+
+// prima versione for each
 // function miaFunzione (array,a,b){
-//
-// const nuovoArray = [];
-//
 // arrayN.forEach((element, i, array) => {
 //   if (i >= a && i <= b) {
 //      nuovoArray.push(array[i]);
 //   }
-//
+// });
 // }
-// );
+//
+// const arrayN = ["rosso", "blu", "verde", "giallo", "bianco"];
+// const nuovoArray = [];
+// const risultato = miaFunzione(arrayN,0,2);
+//
 // console.log(arrayN);
 // console.log(nuovoArray);
+// fine
 
 
+// versione tradizionale
+// function intervallo(array, a, b){
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (i >= a && i <= b) {
+//        newArray.push(array[i]);
+//     }
+// }
+// return newArray;
+// }
+// const arrayN = ["rosso", "blu", "verde", "giallo", "bianco"];
+// const risultato = intervallo(arrayN,0,2);
+// console.log(risultato);
+// fine
 
 
+// versione arrayFilter
 // const arrayN = ["rosso", "blu", "verde", "giallo", "bianco"];
 //
-// const risultato = miaFunzione(arrayN,0,2);
-//
-//
-// // for (let i = 0; i < array.length; i++) {
-// //   if (i >= a && i <= b) {
-// //      nuovoArray.push(array[i]);
-// //   }
-// // }
-// // return nuovoArray;
+// function miaFunzione (array,a,b){
+//   return arrayN.filter((element,i)=> i>= a && i <= b);
 // }
-// // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-
-
-// var filterN = arrayN.filter((element,a,b)=>{
-//   return element>= 0 && element <= 2
-// });
 // const risultato = miaFunzione(arrayN,0,2);
-// console.log(filterN);
+// console.log(risultato);
+// fine
 
+
+// versione filter con numeri
 const arrayN = ["rosso", "blu", "verde", "giallo", "bianco"];
 var min = 2;
 var max = 5;
@@ -46,22 +56,3 @@ return min<= index && max >= index;
 });
 
 console.log(arrayFilter);
-
-// ridotta
-// const arrayFilter = arrayN.filter((element,index)=> min<= index && max >= index);
-
-
-
-// var new = filter(name, min, max);
-//
-// function filter(name, min, max){
-//   return new.slice(arrayN,0,2)
-// }
-
-// const somma =()=> 1+2;
-//
-// // function somma(){
-// //   return 1+1;
-//
-// const result = somma();
-// console.log(result);
